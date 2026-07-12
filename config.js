12d8fc5140f93a0ca0b3990c762f016aa07ec1fe
Dhaniqtz <Chirandanitha652@gmail.com>
2026-07-12 19:42:27 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6bZ2nbwB#wO6UCBsUYY_B-wvTSzRoqygNBibsglFh6uTAhWa8xjc",
+SESSION_ID: process.env.SESSION_ID || "WawhCJqZ#UsA5S18oR0uPFq7-LPbRwfP_l3BOyT6PF6trg2noKmQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Dhaniqtz/DHANI-MD/blob/main/Image/file_00000000d60871fd86e88fa5ba092f9d.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DHANI-MD Is Alive Now😍*",
 BOT_OWNER: '94719002563',  // Replace with the owner's phone number
